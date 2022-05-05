d238e5f37ff90982b64edfd613d8bad7ab9d9c1c
juanluidos <juanluaguilerarive@gmail.com>
2022-05-05 21:15:42 +0200

    Construcción páginas mediante bloques. Rehecho la página de buscarPersona y su formulario. Búsqueda por nickname hecha

--- App.py
+++ App.py
@@ -1,14 +1,37 @@
-from flask import Flask, redirect, url_for, render_template
-
+from django.shortcuts import render
+from flask import Flask, redirect, url_for, render_template, request
+import subprocess
+from username import scraping
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index.html")
 def home():
     return render_template("index.html")
 
 @app.route("/buscarPersona")
 def buscarPersona():
     return render_template("buscarPersona.html")    
+
+@app.route("/result", methods=["POST", "GET"])
+def result():
+
+    if request.method == 'POST':
+        nombre = request.form["nombre"]
+        apellidos = request.form["apellidos"]
+        nickname = request.form["nickname"]
+        email = request.form["email"]
+        city = request.form["city"]
+
+        if nickname:
+            resultadoscript = scraping.usernameScrapping(nickname, './username/web_accounts_list.json')
+            return render_template("resultadosBusqueda.html", nickname=nickname, resultado = resultadoscript)
+        else:
+            return render_template("resultadosBusqueda.html", nombre = nombre, nickname = nickname)
+    else:
+        #quizás añadir 404.html en lugar de redirect
+        return redirect("index.html")
+        
 
 # @app.route("/<name>")
 # def user(name):
---
+++ username/scraping.py
@@ -0,0 +1,126 @@
+import argparse
+import codecs
+from datetime import datetime
+import json
+import os
+import random
+import signal
+import string
+import sys
+import time
+
+import requests
+import urllib3
+import threading
+import logging
+
+
+def usernameScrapping(username, inputfile):
+    #todos los sitios encontrados con username demandado
+    all_found_sites = []
+
+    #Sistema de logs
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate'
+            }
+
+    # Create the final results dictionary
+    username_results = []
+
+    def web_call(location):
+        try:
+            # Make web request for that URL, timeout in X secs and don't verify SSL/TLS certs
+            resp = requests.get(location, headers=headers, timeout=60, verify=False)
+        except requests.exceptions.Timeout:
+            return f' !  ERROR: {location} CONNECTION TIME OUT. Try increasing the timeout delay.'
+        except requests.exceptions.TooManyRedirects:
+            return f' !  ERROR: {location} TOO MANY REDIRECTS. Try changing the URL.'
+        except requests.exceptions.RequestException as e:
+            return f' !  ERROR: CRITICAL ERROR. {e}'
+        else:
+            return resp
+
+
+    def random_string(length):
+        return ''.join(
+            random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for x in range(length))
+
+    # Suppress HTTPS warnings
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+
+    with open(inputfile, 'r', errors='ignore') as data_file:
+        data = json.load(data_file)
+
+    def check_site(site, username):
+        # Examine the current validity of the entry
+        if not site['valid']:
+            return logging.info(f" *  Skipping {site['name']} - Marked as not valid.")
+
+        if not site['known_accounts'][0]:
+            return logging.info(f" *  Skipping {site['name']} - No valid user names to test.")
+
+        # Set the username
+        if username:
+            uname = username
+        else:
+            # if no username specified Pull the first user from known_accounts and replace the {account} with it
+            known_account = site['known_accounts'][0]
+            uname = known_account
+
+        url = site['check_uri'].replace("{account}", uname)
+
+        # Perform initial lookup
+        logging.info(f" >  Looking up {url}")
+        r = web_call(url)
+        if isinstance(r, str):
+            # We got an error on the web call
+            return logging.error(r)
+        else:
+            # Analyze the responses against what they should be
+            code_match = r.status_code == int(site['account_existence_code'])
+            if site['account_existence_string']:
+                string_match = r.text.find(site['account_existence_string']) >= 0
+            else:
+                string_match = 0
+
+            if username:
+                if code_match and string_match:
+                    if 'pretty_uri' in site:
+                        url = site['pretty_uri'].replace("{account}", uname)
+                        
+                    username_results.append('[+] Found user at %s' % url)
+                    all_found_sites.append(url)
+                    return
+            else:
+                if code_match and string_match:
+                    # logging.info('     [+] Response code and Search Strings match expected.')
+                    # Generate a random string to use in place of known_accounts
+                    url_fp = site['check_uri'].replace("{account}", random_string(20))
+                    r_fp = web_call(url_fp)
+                    if isinstance(r_fp, str):
+                        # If this is a string then web got an error
+                        return logging.error(r_fp)
+
+                    code_match = r_fp.status_code == int(site['account_existence_code'])
+                    string_match = r_fp.text.find(site['account_existence_string']) > 0
+
+    # Start threads
+    threads = []
+
+    for site_ in data['sites']:
+        x = threading.Thread(target=check_site, args=(site_, username), daemon=True)
+        threads.append(x)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    return (all_found_sites, username_results)
+
